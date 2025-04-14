02791f4b671a34c59345d2919f0924842c242c3a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-14 23:48:15 +0200

    Initial Commit

---
+++ src/client/java/top/offsetmonkey538/rainbowwood/RainbowWoodClient.java
@@ -0,0 +1,11 @@
+package top.offsetmonkey538.rainbowwood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RainbowWoodClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// Do stuff
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/rainbowwood/RainbowWood.java
@@ -0,0 +1,25 @@
+package top.offsetmonkey538.rainbowwood;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import top.offsetmonkey538.rainbowwood.block.ModBlocks;
+
+import top.offsetmonkey538.rainbowwood.item.ModItems;
+
+public class RainbowWood implements ModInitializer {
+	public static final String MOD_ID = "rainbow-wood";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModBlocks.register();
+		ModItems.register();
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
