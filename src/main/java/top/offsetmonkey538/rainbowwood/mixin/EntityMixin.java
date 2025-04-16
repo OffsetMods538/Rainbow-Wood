b9975db63d4d55739d1fd86cfe799a6c87c24057
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-16 23:48:10 +0200

    Allow "washing" all logs and planks (everything in vanilla logs and planks tags)

--- src/main/java/top/offsetmonkey538/rainbowwood/mixin/EntityMixin.java
+++ src/main/java/top/offsetmonkey538/rainbowwood/mixin/EntityMixin.java
@@ -4,12 +4,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.offsetmonkey538.rainbowwood.block.ModBlocks;
 import top.offsetmonkey538.rainbowwood.component.ModComponents;
 import top.offsetmonkey538.rainbowwood.item.TintedBlockItem;
 
@@ -22,12 +28,32 @@ public abstract class EntityMixin {
     )
     private void rainbow_wood$clearWoodColorWhenCollidingWithCauldronWithWater(BlockState state, CallbackInfo ci) {
         if (!state.isOf(Blocks.WATER_CAULDRON)) return;
-        final Entity thisEntity = (Entity) (Object) this;
-        if (!(thisEntity instanceof ItemEntity itemEntity) || !(itemEntity.getStack().getItem() instanceof TintedBlockItem)) return;
+        final Entity thisEntity = (Entity) (Object) this; // Yes VVVV I am casting Item to Object and then checking if it's an Item. The return value is nullable so this is a null-check with the benefit of getting the instance haha
+        if (!(thisEntity instanceof ItemEntity itemEntity) || !((Object) rainbow_wood$getMatchingTintableBlockItemFor(itemEntity.getStack()) instanceof Item newItem)) return;
 
-        if (itemEntity.getStack().get(ModComponents.TINT_COLOR) == null) return;
-        itemEntity.getStack().remove(ModComponents.TINT_COLOR);
+        final ItemStack newStack = itemEntity.getStack().copyComponentsToNewStack(newItem, itemEntity.getStack().getCount());
+        if (newStack.get(ModComponents.TINT_COLOR) == null && itemEntity.getStack().isOf(newItem)) return;
+        newStack.remove(ModComponents.TINT_COLOR);
+        itemEntity.setStack(newStack);
 
         thisEntity.getWorld().playSound(thisEntity, thisEntity.getBlockPos(), SoundEvents.ENTITY_GENERIC_SPLASH, SoundCategory.AMBIENT, 1, 1);
+    }
+
+    @Unique
+    private @Nullable Item rainbow_wood$getMatchingTintableBlockItemFor(final ItemStack item) {
+        if (item.getItem() instanceof TintedBlockItem tintedBlockItem) return tintedBlockItem;
+        if (item.isIn(ItemTags.LOGS)) return ModBlocks.RAINBOW_LOG.asItem();
+        if (item.isIn(ItemTags.PLANKS)) return ModBlocks.RAINBOW_PLANKS.asItem();
+        //TODO: if (item.isIn(ItemTags.SIGNS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.HANGING_SIGNS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_BUTTONS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_DOORS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_STAIRS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_SLABS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_FENCES)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_PRESSURE_PLATES)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.WOODEN_TRAPDOORS)) return ModBlocks.RAINBOW_LOG.asItem();
+        //TODO: if (item.isIn(ItemTags.FENCE_GATES)) return ModBlocks.RAINBOW_LOG.asItem();
+        return null;
     }
 }
