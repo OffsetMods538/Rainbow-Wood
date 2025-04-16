e59b2d854c6562b733c090935b0ee4344d7883b4
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-16 11:01:17 +0200

    remove chunk data attachment stuff

--- src/client/java/top/offsetmonkey538/rainbowwood/RainbowWoodClient.java
+++ src/client/java/top/offsetmonkey538/rainbowwood/RainbowWoodClient.java
@@ -2,11 +2,6 @@ package top.offsetmonkey538.rainbowwood;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.world.chunk.Chunk;
-import top.offsetmonkey538.rainbowwood.attachment.ModAttachmentTypes;
-import top.offsetmonkey538.rainbowwood.attachment.ModBlockTintAttachedData;
 import top.offsetmonkey538.rainbowwood.block.ModBlocks;
 import top.offsetmonkey538.rainbowwood.component.ModComponents;
 
@@ -15,14 +10,7 @@ public class RainbowWoodClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		ColorProviderRegistry.BLOCK.register((state, world1, pos, tintIndex) -> {
-			if (pos == null) return 0;
-			final ClientWorld world = MinecraftClient.getInstance().world;
-			if (world == null) return 0;
-			final Chunk chunk = world.getChunk(pos);
-			//MinecraftClient.getInstance().world.getChunk()
-            final ModBlockTintAttachedData tint = chunk.getAttachedOrElse(ModAttachmentTypes.BLOCK_TINT_TYPE, ModBlockTintAttachedData.DEFAULT);
-
-			return tint.getTint(pos.getX() - chunk.getPos().getStartX(), pos.getY(), pos.getZ() - chunk.getPos().getStartZ());
+			return 0x7f6a5e;
 		}, ModBlocks.RAINBOW_PLANKS);
 
 		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
