f39102db4969b915dc8d30a1e1296e6b7de16dce
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-16 14:34:59 +0200

    Add color as tooltip to planks item

--- src/client/java/top/offsetmonkey538/rainbowwood/datagen/RainbowWoodDatagen.java
+++ src/client/java/top/offsetmonkey538/rainbowwood/datagen/RainbowWoodDatagen.java
@@ -2,6 +2,7 @@ package top.offsetmonkey538.rainbowwood.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import top.offsetmonkey538.rainbowwood.datagen.language.ModEnglishLanguageProvider;
 
 public class RainbowWoodDatagen implements DataGeneratorEntrypoint {
 
@@ -9,5 +10,7 @@ public class RainbowWoodDatagen implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         final FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(ModModelProvider::new);
+
+        pack.addProvider(ModEnglishLanguageProvider::new);
     }
 }
--- src/main/java/top/offsetmonkey538/rainbowwood/item/TintedBlockItem.java
+++ src/main/java/top/offsetmonkey538/rainbowwood/item/TintedBlockItem.java
@@ -3,7 +3,11 @@ package top.offsetmonkey538.rainbowwood.item;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.text.Text;
 import top.offsetmonkey538.rainbowwood.component.ModComponents;
+
+import java.util.List;
 
 public class TintedBlockItem extends BlockItem {
     public TintedBlockItem(Block block, Settings settings) {
@@ -14,5 +18,14 @@ public class TintedBlockItem extends BlockItem {
         final ItemStack result = getDefaultStack();
         result.set(ModComponents.TINT_COLOR, tint);
         return result;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        super.appendTooltip(stack, context, tooltip, type);
+        final Integer tintColor = stack.get(ModComponents.TINT_COLOR);
+        if (tintColor == null) return;
+
+        tooltip.add(Text.translatable("general.rainbow_wood.tooltip.color", Text.literal("#%06X".formatted(tintColor)).withColor(tintColor)));
     }
 }
