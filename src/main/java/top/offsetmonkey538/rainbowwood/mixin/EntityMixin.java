b4c057248f94e1fb477a336b93663904cb1e76b7
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-18 13:58:11 +0200

    Differentiate between logs, stripped logs, wood and stripped wood when washing in cauldron

--- src/main/java/top/offsetmonkey538/rainbowwood/mixin/EntityMixin.java
+++ src/main/java/top/offsetmonkey538/rainbowwood/mixin/EntityMixin.java
@@ -1,14 +1,19 @@
 package top.offsetmonkey538.rainbowwood.mixin;
 
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,6 +23,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.offsetmonkey538.rainbowwood.block.ModBlocks;
 import top.offsetmonkey538.rainbowwood.component.ModComponents;
 import top.offsetmonkey538.rainbowwood.item.TintedBlockItem;
+
+import java.util.Optional;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
@@ -42,7 +49,7 @@ public abstract class EntityMixin {
     @Unique
     private @Nullable Item rainbow_wood$getMatchingTintableBlockItemFor(final ItemStack item) {
         if (item.getItem() instanceof TintedBlockItem tintedBlockItem) return tintedBlockItem;
-        if (item.isIn(ItemTags.LOGS)) return ModBlocks.RAINBOW_LOG.asItem(); // TODO: logs tag includes stripped and wood, which are their own blocks
+        if (item.isIn(ItemTags.LOGS)) return rainbow_wood$getMatchingTintableBlockItemForLog(item);
         if (item.isIn(ItemTags.PLANKS)) return ModBlocks.RAINBOW_PLANKS.asItem();
         if (item.isIn(ItemTags.WOODEN_SLABS)) return ModBlocks.RAINBOW_SLAB.asItem();
         //TODO: if (item.isIn(ItemTags.SIGNS)) return ModBlocks.RAINBOW_SIGN.asItem();
@@ -55,5 +62,20 @@ public abstract class EntityMixin {
         //TODO: if (item.isIn(ItemTags.WOODEN_PRESSURE_PLATES)) return ModBlocks.RAINBOW_PRESSURE_PLATE.asItem();
         //TODO: if (item.isIn(ItemTags.WOODEN_TRAPDOORS)) return ModBlocks.RAINBOW_TRAPDOOR.asItem();
         return null;
+    }
+
+    @Unique
+    private @NotNull Item rainbow_wood$getMatchingTintableBlockItemForLog(final ItemStack item) {
+        if (item.isIn(ConventionalItemTags.STRIPPED_LOGS)) return ModBlocks.STRIPPED_RAINBOW_LOG.asItem();
+        if (item.isIn(ConventionalItemTags.STRIPPED_WOODS)) return ModBlocks.STRIPPED_RAINBOW_WOOD.asItem();
+
+        if (!(item.getItem() instanceof BlockItem blockItem)) return ModBlocks.RAINBOW_LOG.asItem();
+
+        final Optional<Block> optionalStrippedVariant = Optional.ofNullable(AxeItem.STRIPPED_BLOCKS.get(blockItem.getBlock()));
+        if (optionalStrippedVariant.isEmpty()) return ModBlocks.RAINBOW_LOG.asItem();
+
+        if (optionalStrippedVariant.get().asItem().getDefaultStack().isIn(ConventionalItemTags.STRIPPED_WOODS)) return ModBlocks.RAINBOW_WOOD.asItem();
+
+        return ModBlocks.RAINBOW_LOG.asItem();
     }
 }
