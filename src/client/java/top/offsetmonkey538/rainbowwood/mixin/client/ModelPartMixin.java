3b252ac65c945ece09e193e23538ff1b9f6e783a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-23 23:15:53 +0300

    Store reverse map for children of hanging sign model parts
    Should probably be faster than going through every entry every time and checking if they're equal?

--- src/client/java/top/offsetmonkey538/rainbowwood/mixin/client/ModelPartMixin.java
+++ src/client/java/top/offsetmonkey538/rainbowwood/mixin/client/ModelPartMixin.java
@@ -8,16 +8,20 @@ import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import top.offsetmonkey538.rainbowwood.util.SignRenderContext;
 
-import java.util.Map;
+import java.util.*;
 
 @Mixin(ModelPart.class)
 public abstract class ModelPartMixin {
 
     @Shadow @Final private Map<String, ModelPart> children;
+
+    @Unique
+    private Map<ModelPart, String> rainbow_wood$reverseChildrenMap = null;
 
     @ModifyArg(
             method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V",
@@ -45,13 +49,13 @@ public abstract class ModelPartMixin {
             return;
         }
 
-        // TODO: I should probably cache a reverse-map of children and use that but ehhhhh it's fine for now
-        final String name = children
-                .entrySet()
-                .stream()
-                .filter(entry -> instance.equals(entry.getValue()))
-                .map(Map.Entry::getKey)
-                .findFirst().orElse("");
+        if (rainbow_wood$reverseChildrenMap == null) {
+            rainbow_wood$reverseChildrenMap = children
+                    .entrySet()
+                    .stream()
+                    .collect(HashMap::new, (map, entry) -> map.put(entry.getValue(), entry.getKey()), HashMap::putAll);
+        }
+        final String name = rainbow_wood$reverseChildrenMap.get(instance);
 
         original.call(instance, matrices, vertices, light, overlay, name.toUpperCase().contains("CHAIN") ? -1 : SignRenderContext.color.get());
     }
