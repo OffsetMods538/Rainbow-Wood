d6567584705cf1bd9b5b13ec96a290a3a0884fdc
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-23 22:41:22 +0300

    Generate item group content at startup
    May be faster than generating it every time the creative menu is opened?

--- src/main/java/top/offsetmonkey538/rainbowwood/item/group/ModItemGroups.java
+++ src/main/java/top/offsetmonkey538/rainbowwood/item/group/ModItemGroups.java
@@ -3,6 +3,7 @@ package top.offsetmonkey538.rainbowwood.item.group;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -11,6 +12,9 @@ import top.offsetmonkey538.rainbowwood.item.ITintedBlockItem;
 import top.offsetmonkey538.rainbowwood.item.ModItems;
 import top.offsetmonkey538.rainbowwood.util.NamedColor;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import static top.offsetmonkey538.rainbowwood.RainbowWood.id;
 
 public final class ModItemGroups {
@@ -18,19 +22,31 @@ public final class ModItemGroups {
 
     }
 
+    private static final ItemStack[] RAINBOW_WOOD_ITEM_GROUP_CONTENT;
+
+    static {
+        final List<ItemStack> content = new LinkedList<>();
+
+        for (Item item : ModItems.ITEMS) {
+            content.add(item.getDefaultStack());
+        }
+
+        for (final NamedColor color : NamedColor.getAllColors()) {
+            for (Item item : ModItems.ITEMS) {
+                content.add(((ITintedBlockItem) item).getStackWithTint(color.color));
+            }
+        }
+
+        RAINBOW_WOOD_ITEM_GROUP_CONTENT = content.toArray(ItemStack[]::new);
+    }
+
     public static final ItemGroup RAINBOW_WOOD_ITEM_GROUP = register("rainbow_wood_item_group",
             FabricItemGroup.builder()
                     .icon(() -> ((ITintedBlockItem) ModBlocks.RAINBOW_PLANKS.asItem()).getStackWithTint(0xFF00FF)) // todo: Find some nice color for mod and item group icon.
                     .displayName(Text.translatable("itemGroup.rainbow_wood.main_group"))
                     .entries((displayContext, entries) -> {
-                        for (Item item : ModItems.ITEMS) {
-                            entries.add(item.getDefaultStack());
-                        }
-
-                        for (final NamedColor color : NamedColor.getAllColors()) {
-                            for (Item item : ModItems.ITEMS) {
-                                entries.add(((ITintedBlockItem) item).getStackWithTint(color.color));
-                            }
+                        for (ItemStack item : RAINBOW_WOOD_ITEM_GROUP_CONTENT) {
+                            entries.add(item);
                         }
                     })
                     .build()
