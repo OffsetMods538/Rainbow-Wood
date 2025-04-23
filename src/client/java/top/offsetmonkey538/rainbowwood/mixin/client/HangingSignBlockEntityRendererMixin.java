d5d9d0c413b887c71883c5da660220ba94739e2a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-23 23:05:35 +0300

    Don't render an oak hanging sign model in addition to the rainbow one.
    I think I was at one point trying to render the chains without tint by having the chain texture removed from the rainbow texture and then rendering the oak hanging sign model underneath

--- src/client/java/top/offsetmonkey538/rainbowwood/mixin/client/HangingSignBlockEntityRendererMixin.java
+++ src/client/java/top/offsetmonkey538/rainbowwood/mixin/client/HangingSignBlockEntityRendererMixin.java
@@ -4,46 +4,17 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import net.minecraft.block.AbstractSignBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.WoodType;
-import net.minecraft.block.entity.SignBlockEntity;
-import net.minecraft.client.model.Model;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.HangingSignBlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import top.offsetmonkey538.rainbowwood.client.render.block.entity.TintedHangingSignBlockEntityRenderer;
 import top.offsetmonkey538.rainbowwood.util.SignRenderContext;
 
-import java.util.Map;
-
 @Mixin(HangingSignBlockEntityRenderer.class)
 public abstract class HangingSignBlockEntityRendererMixin {
-
-    @Shadow @Final private Map<WoodType, HangingSignBlockEntityRenderer.HangingSignModel> MODELS;
-
-    @Unique
-    private HangingSignBlockEntityRenderer.HangingSignModel rainbow_wood$oakModel = null;
-
-    @WrapOperation(
-            method = "render(Lnet/minecraft/block/entity/SignBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/block/entity/HangingSignBlockEntityRenderer;render(Lnet/minecraft/block/entity/SignBlockEntity;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/block/BlockState;Lnet/minecraft/block/AbstractSignBlock;Lnet/minecraft/block/WoodType;Lnet/minecraft/client/model/Model;)V")
-    )
-    private void rainbow_wood$setColorForModelPartRenderWhenRainbowSign(HangingSignBlockEntityRenderer instance, SignBlockEntity signBlockEntity, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, BlockState blockState, AbstractSignBlock abstractSignBlock, WoodType woodType, Model model, Operation<Void> original) {
-        if ((HangingSignBlockEntityRenderer) (Object) this instanceof TintedHangingSignBlockEntityRenderer) {
-            rainbow_wood$oakModel = MODELS.get(WoodType.OAK);
-            rainbow_wood$oakModel.updateVisibleParts(blockState);
-        }
-
-        original.call(instance, signBlockEntity, matrixStack, vertexConsumerProvider, i, j, blockState, abstractSignBlock, woodType, model);
-    }
 
     @WrapOperation(
             method = "renderSignModel",
@@ -55,7 +26,6 @@ public abstract class HangingSignBlockEntityRendererMixin {
             return;
         }
 
-        if (rainbow_wood$oakModel != null) rainbow_wood$oakModel.render(matrices, vertices, light, overlay);
         SignRenderContext.contextualize(() -> original.call(instance, matrices, vertices, light, overlay), tintedEntityRenderer.getTint());
     }
 }
