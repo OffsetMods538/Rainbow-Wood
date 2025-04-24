88d680beee1b4d3ac3f041bc78b297598b66a460
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-24 14:36:32 +0300

    Remove access widener for BlockSetType.register

--- src/main/java/top/offsetmonkey538/rainbowwood/block/ModBlockSetTypes.java
+++ src/main/java/top/offsetmonkey538/rainbowwood/block/ModBlockSetTypes.java
@@ -1,13 +1,16 @@
 package top.offsetmonkey538.rainbowwood.block;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.type.BlockSetTypeBuilder;
 import net.minecraft.block.BlockSetType;
+
+import static top.offsetmonkey538.rainbowwood.RainbowWood.id;
 
 public final class ModBlockSetTypes {
     private ModBlockSetTypes() {
 
     }
 
-    public static final BlockSetType RAINBOW = BlockSetType.register(new BlockSetType("rainbow"));
+    public static final BlockSetType RAINBOW = BlockSetTypeBuilder.copyOf(new BlockSetType("")).register(id("rainbow"));
 
     public static void register() {
         // Registers block set types by loading the class.
