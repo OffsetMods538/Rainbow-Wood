cc205e8dfbb36f52e3a83a3cd78e685e56100972
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-24 21:07:51 +0300

    Add EMI

---
+++ src/client/java/top/offsetmonkey538/rainbowwood/compat/emi/ModEmiPlugin.java
@@ -0,0 +1,12 @@
+package top.offsetmonkey538.rainbowwood.compat.emi;
+
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+
+public class ModEmiPlugin implements EmiPlugin {
+
+    @Override
+    public void register(EmiRegistry emiRegistry) {
+
+    }
+}
