0c6a3ce9babb5bb5767cf2a3ab8d57576af88cd1
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-04-25 22:59:35 +0300

    Add boat type

---
+++ src/main/java/top/offsetmonkey538/rainbowwood/fasm/RainbowWoodEarlyRiser.java
@@ -0,0 +1,23 @@
+package top.offsetmonkey538.rainbowwood.fasm;
+
+import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
+import net.minecraft.entity.vehicle.BoatEntity;
+import top.offsetmonkey538.rainbowwood.block.ModBlocks;
+
+public class RainbowWoodEarlyRiser implements Runnable {
+    @Override
+    public void run() {
+        final MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
+
+        final String boatEntityType = remapper.mapClassName("intermediary", "net.minecraft.class_1690$class_1692");
+        final String block = "L%s;".formatted(remapper.mapClassName("intermediary", "net.minecraft.class_2248"));
+        final String string = "Ljava/lang/String;";
+        ClassTinkerers.enumBuilder(boatEntityType, block, string).addEnum("RAINBOW", () -> new Object[] {ModBlocks.RAINBOW_PLANKS, "rainbow"}).build();
+    }
+
+    public static BoatEntity.Type getRainbowType() {
+        return ClassTinkerers.getEnum(BoatEntity.Type.class, "RAINBOW");
+    }
+}
